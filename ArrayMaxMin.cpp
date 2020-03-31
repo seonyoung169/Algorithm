972cdd1df5b24caa47055deebe34328ebd724698
sunnyangee <40056233+seonyoung169@users.noreply.github.com>
2020-03-31 13:23:09 +0900

    Create ArrayMaxMin.cpp

---
+++ ArrayMaxMin.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void getMaxMin(int N, int * numArray);
+
+int main() {
+    //setup
+    int N, num;
+    cin >> N;
+
+    int * numArray = new int[N];
+    for(int i =0; i<N; i++){
+        cin>>num;
+        numArray[i] = num;
+    }
+
+    getMaxMin(N, numArray);
+    return 0;
+}
+
+void getMaxMin(int N, int * numArray){
+    int max = numArray[0], min = numArray[0];
+
+    for(int i=1;i<N;i++){
+        if(numArray[i] > max) max = numArray[i];
+        if(numArray[i] < min) min = numArray[i];
+    }
+
+    cout<<min<<" "<<max<<endl;
+}
