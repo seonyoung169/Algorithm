3e923933892ac60e119f33f056c42f6f19a2a386
sunnyangee <40056233+seonyoung169@users.noreply.github.com>
2020-09-08 16:24:56 +0900

    Create Tiling2N_2.cpp

---
+++ BOJ/Tiling2N_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+//BOJ 11727 타일링 문제 - dynamic programming
+using namespace std;
+
+int tile[1001];
+
+int dp(int n){
+    if(n==1) return 1;
+    if(n==2) return 3;
+    if(tile[n] != 0) return tile[n];
+
+    return ( tile[n] = (dp(n-1) + 2* dp(n-2)) % 10007 );
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin>>N;
+
+    cout<<dp(N)<<'\n';
+
+    return 0;
+}
